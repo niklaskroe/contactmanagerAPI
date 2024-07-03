c03145fa116bfc6a72b0ad7434b9657f8063490b
Niklas <78739302+niklaskroe@users.noreply.github.com>
2024-07-03 08:49:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'H63(ua)rtL'
+app.config['MYSQL_PASSWORD'] = '12345678'
 app.config['MYSQL_DB'] = 'contact_db'
 
 mysql = MySQL(app)
